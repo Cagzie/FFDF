4d0804d34738a2b0493fba740777c18533494231
Cagzie <jack@cagzie.com>
2022-03-22 12:37:43 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FFDF'
+copyright = '2022 Cagzie'
+author = 'Cagzie'
 
 release = '0.1'
 version = '0.1.0'
